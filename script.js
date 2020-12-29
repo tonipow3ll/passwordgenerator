436eddd88a66a056c5e0df9ec7ab92df26772ac7
Toni Powell <tonipow3ll@gmail.com>
2020-12-29 16:14:53 -0500

    debugging/testing

--- script.js
+++ script.js
@@ -5,24 +5,25 @@
 
 // Write password to the #password input
 function writePassword() {
-let complexity = document.getElementById("userchoicenumber").value;
-let values = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890";
+    // add slider here also, below links number input to value
+    let complexity = document.getElementById("usernumber").value;
 
-let password = "";
+    // need to add acceptable symbols to values string
+    let values = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890";
+    
+    let password = "";
+    
+    // loop to generate password
+    for(var i = 1; i <= complexity; i++){
+        password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)));
+        
+        return password;
+    }
+    console.log(writePassword)
+    const area = document.getElementById("displayPW").value = password;
 
-// loop to generate password
-for(var i = 0; i <= complexity; i++){
-    password = password + values.charAt(Math.floor(Math.random() * Math.floor(value.length - 1)));
-}
 //   add password to textbox / display area
 
-document.getElementById(finalPassword).value = password;
-
-
-
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
 
 }
 
