a563f8fbec25c56297e1f8281e42635150f40da4
Toni Powell <tonipow3ll@gmail.com>
2020-12-29 16:33:21 -0500

    more testing

--- script.js
+++ script.js
@@ -2,30 +2,28 @@
 // var generateBtn = document.querySelector("#generate");
 
 // input.value = writePassword();
+document.getElementById("displayPW");
+const area = writePassword();
 
 // Write password to the #password input
 function writePassword() {
     // add slider here also, below links number input to value
     let complexity = document.getElementById("usernumber").value;
 
-    // need to add acceptable symbols to values string
+    
     let values = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890";
     
     let password = "";
     
     // loop to generate password
-    for(var i = 1; i <= complexity; i++){
+    for(var i = 0; i <= complexity; i++){
         password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)));
         
         return password;
     }
-    console.log(writePassword)
-    const area = document.getElementById("displayPW").value = password;
 
-//   add password to textbox / display area
-
-
+    //   add password to textbox / display area 
+    const area = document.getElementById("displayPW").value;
 }
-
 // Add event listener to generate button
-// generateBtn.addEventListener("click", writePassword);
+// generateBtn.addEventListener("click", writePassword)
