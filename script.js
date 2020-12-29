1edba65ce8fb179f4430ceb93b02cbd98dcf15ac
Toni Powell <tonipow3ll@gmail.com>
2020-12-29 15:00:05 -0500

    updated JS

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+input.value = writePassword();
 // Write password to the #password input
 function writePassword() {
+const availableCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890"
+
   var password = generatePassword();
+
+
+
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
